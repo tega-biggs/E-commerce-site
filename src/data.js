8f89702068a7f0461b0db1d794d56dcb1e4badb6
Tega Biggs <oteribiggs1@gmail.com>
2024-04-19 09:37:28 +0100

    Add initial product data and structure for store and detail views

---
+++ src/data.js
@@ -0,0 +1,41 @@
+export const storeProducts = [
+    {
+        id: 1,
+        title: "Google Pixel 4 - Black",
+        img: "img/Google Pixel 4 (1).jpg",
+        price: 10,
+        company: "GOOGLE",
+        info: 
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam non dolor sed risus volutpat semper et eget eros. Aenean at risus euismod, fringilla odio id, feugiat nisl. Aenean sollicitudin leo ac sem mollis pharetra id quis ipsum. Suspendisse potenti. Nullam hendrerit mauris in lectus faucibus mattis at sed turpis. Ut ac suscipit odio, pellentesque commodo dui. Proin volutpat viverra dui, eget cursus mi accumsan eu. Aenean quis eros scelerisque, finibus mi sit amet, cursus diam. Pellentesque varius nibh nec lacus rhoncus commodo. Fusce suscipit porttitor magna eget volutpat. Cras in augue et metus rhoncus condimentum. Fusce sagittis elit et tempus ultricies. Maecenas bibendum turpis et ligula elementum, sed vestibulum risus varius. Suspendisse dui justo, hendrerit vestibulum tristique id, egestas at leo. Pellentesque feugiat libero vel eros scelerisque luctus dapibus eget dui.",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 2,
+        title: "Samsung s7",
+        img: "img/Samsung-Galaxy-S7 (1).jpg",
+        price: 10,
+        company: "Samsung",
+        info: 
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam non dolor sed risus volutpat semper et eget eros. Aenean at risus euismod, fringilla odio id, feugiat nisl. Aenean sollicitudin leo ac sem mollis pharetra id quis ipsum. Suspendisse potenti. Nullam hendrerit mauris in lectus faucibus mattis at sed turpis. Ut ac suscipit odio, pellentesque commodo dui. Proin volutpat viverra dui, eget cursus mi accumsan eu. Aenean quis eros scelerisque, finibus mi sit amet, cursus diam. Pellentesque varius nibh nec lacus rhoncus commodo. Fusce suscipit porttitor magna eget volutpat. Cras in augue et metus rhoncus condimentum. Fusce sagittis elit et tempus ultricies. Maecenas bibendum turpis et ligula elementum, sed vestibulum risus varius. Suspendisse dui justo, hendrerit vestibulum tristique id, egestas at leo. Pellentesque feugiat libero vel eros scelerisque luctus dapibus eget dui.",
+        inCart: false,
+        count: 0,
+        total: 0
+    }
+]
+
+export const detailProduct = {
+    id: 1,
+    title: "Google Pixel - Black",
+    img: "img/black-google-pixel (1).jpg",
+    price: 10,
+    company: "google",
+    info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam non dolor sed risus volutpat semper et eget eros. Aenean at risus euismod, fringilla odio id, feugiat nisl. Aenean sollicitudin leo ac sem mollis pharetra id quis ipsum. Suspendisse potenti. Nullam hendrerit mauris in lectus faucibus mattis at sed turpis. Ut ac suscipit odio, pellentesque commodo dui. Proin volutpat viverra dui, eget cursus mi accumsan eu. Aenean quis eros scelerisque, finibus mi sit amet, cursus diam. Pellentesque varius nibh nec lacus rhoncus commodo. Fusce suscipit porttitor magna eget volutpat. Cras in augue et metus rhoncus condimentum. Fusce sagittis elit et tempus ultricies. Maecenas bibendum turpis et ligula elementum, sed vestibulum risus varius. Suspendisse dui justo, hendrerit vestibulum tristique id, egestas at leo. Pellentesque feugiat libero vel eros scelerisque luctus dapibus eget dui.",
+    inCart: false,
+    count: 0,
+    total: 0
+
+}
+
+
