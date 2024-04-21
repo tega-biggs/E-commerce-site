fe83515fab7a75ccbde59c90d8ea29b09c04188d
Tega Biggs <oteribiggs1@gmail.com>
2024-04-21 10:37:33 +0100

    Create details component for products with image and info

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,11 +1,54 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { ProductConsumer } from '../context';
+import {Link} from 'react-router-dom';
+import {ButtonContainer} from './Button';
 
 export default class Details extends Component {
   render() {
     return (
-      <div>
-        <h3>Hello from details</h3>
-      </div>
+      <ProductConsumer>
+        { value => {
+          const {
+            id,
+            company,
+            img,
+            info,
+            price,
+            title,
+            inCart
+          } = value.detailProduct;
+          return (
+            <div className ='container py-5'>
+              {/*title*/}
+              <div className='container py-5'>
+                <div className='col-10 mx-auto text-center text-slanted text-blue my-5'>
+                  <h1>{title}</h1>
+                </div>
+              </div>
+              {/*end title*/}
+              {/*product info*/}
+              <div className='row'>
+              <div className='col-10 mx-auto col-md-6 my-3'>
+              <img src={img} className='img-fluid' alt='product'></img>   
+              </div>
+              {/* product text */}
+              <div className='col-10 mx-auto col-md-6 my-3 text-capitalize'>
+                <h2>model : {title}</h2>
+                <h4 className='text-title text-uppercase text-muted mt-3 mb-2'>
+                  made by : <span className='text-uppercase'>{company}</span>
+                </h4>
+                <h4 className='text-blue'>
+                  <strong>
+                    price : <span>₦</span>
+                    {price}
+                  </strong>
+                </h4>
+              </div>
+              </div>
+            </div>
+          )
+        }}
+      </ProductConsumer>
     )
   }
 }
