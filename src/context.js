093d7db3cd5b64672b555a95761148ada9e7c395
Tega Biggs <oteribiggs1@gmail.com>
2024-04-22 10:33:50 +0100

    Refactor product data initialization

--- src/context.js
+++ src/context.js
@@ -4,9 +4,23 @@ const ProductContext = React.createContext();
 
  class ProductProvider extends Component {
     state = {
-        products:storeProducts,
-        detailProduct:detailProduct
+        products: [],
+        detailProduct: detailProduct
+    };
+    componentDidMount(){
+        this.setProducts();
     }
+    setProducts = () =>{ 
+        let tempProducts = [];
+        storeProducts.forEach(item =>{
+            const singleItem = {...item};
+            tempProducts = [...tempProducts,singleItem];
+        })
+        this.setState(() => {
+            return {products:tempProducts}
+        })
+    }
+
     handleDetail = () => {
         console.log('hello from detail');
     }
