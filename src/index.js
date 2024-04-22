3f64bec077ece0ce7cd6cf6434fdc61fe115cdd2
Tega Biggs <oteribiggs1@gmail.com>
2024-04-22 10:28:33 +0100

    Import product provider module

--- src/index.js
+++ src/index.js
@@ -3,14 +3,18 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { ProductProvider } from './context';
 import reportWebVitals from './reportWebVitals';
+
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ProductProvider>
     <Router>
      <App />
     </Router>
+    </ProductProvider>
   </React.StrictMode>
 );
 
