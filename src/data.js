c8f576dfd2c9afe802b8b320fd6181d0e82bd746
Tega Biggs <oteribiggs1@gmail.com>
2024-04-22 10:31:06 +0100

    Add more product data

--- src/data.js
+++ src/data.js
@@ -1,8 +1,8 @@
 export const storeProducts = [
     {
         id: 1,
-        title: "Google Pixel 4 - Black",
-        img: "img/Google Pixel 4 (1).jpg",
+        title: "Accer - Black",
+        img: "img/accer-laptop (1).jpg",
         price: 10,
         company: "GOOGLE",
         info: 
@@ -13,10 +13,82 @@ export const storeProducts = [
     },
     {
         id: 2,
-        title: "Samsung s7",
-        img: "img/Samsung-Galaxy-S7 (1).jpg",
+        title: "Asus",
+        img: "img/asus-laptop (1).jpg",
         price: 10,
         company: "Samsung",
+        info: 
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam non dolor sed risus volutpat semper et eget eros. Aenean at risus euismod, fringilla odio id, feugiat nisl. Aenean sollicitudin leo ac sem mollis pharetra id quis ipsum. Suspendisse potenti. Nullam hendrerit mauris in lectus faucibus mattis at sed turpis. Ut ac suscipit odio, pellentesque commodo dui. Proin volutpat viverra dui, eget cursus mi accumsan eu. Aenean quis eros scelerisque, finibus mi sit amet, cursus diam. Pellentesque varius nibh nec lacus rhoncus commodo. Fusce suscipit porttitor magna eget volutpat. Cras in augue et metus rhoncus condimentum. Fusce sagittis elit et tempus ultricies. Maecenas bibendum turpis et ligula elementum, sed vestibulum risus varius. Suspendisse dui justo, hendrerit vestibulum tristique id, egestas at leo. Pellentesque feugiat libero vel eros scelerisque luctus dapibus eget dui.",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 3,
+        title: "Dell - Black",
+        img: "img/dell (1).jpg",
+        price: 10,
+        company: "GOOGLE",
+        info: 
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam non dolor sed risus volutpat semper et eget eros. Aenean at risus euismod, fringilla odio id, feugiat nisl. Aenean sollicitudin leo ac sem mollis pharetra id quis ipsum. Suspendisse potenti. Nullam hendrerit mauris in lectus faucibus mattis at sed turpis. Ut ac suscipit odio, pellentesque commodo dui. Proin volutpat viverra dui, eget cursus mi accumsan eu. Aenean quis eros scelerisque, finibus mi sit amet, cursus diam. Pellentesque varius nibh nec lacus rhoncus commodo. Fusce suscipit porttitor magna eget volutpat. Cras in augue et metus rhoncus condimentum. Fusce sagittis elit et tempus ultricies. Maecenas bibendum turpis et ligula elementum, sed vestibulum risus varius. Suspendisse dui justo, hendrerit vestibulum tristique id, egestas at leo. Pellentesque feugiat libero vel eros scelerisque luctus dapibus eget dui.",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 4,
+        title: "Hp - Black",
+        img: "img/hp-laptop (1).jpg",
+        price: 10,
+        company: "GOOGLE",
+        info: 
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam non dolor sed risus volutpat semper et eget eros. Aenean at risus euismod, fringilla odio id, feugiat nisl. Aenean sollicitudin leo ac sem mollis pharetra id quis ipsum. Suspendisse potenti. Nullam hendrerit mauris in lectus faucibus mattis at sed turpis. Ut ac suscipit odio, pellentesque commodo dui. Proin volutpat viverra dui, eget cursus mi accumsan eu. Aenean quis eros scelerisque, finibus mi sit amet, cursus diam. Pellentesque varius nibh nec lacus rhoncus commodo. Fusce suscipit porttitor magna eget volutpat. Cras in augue et metus rhoncus condimentum. Fusce sagittis elit et tempus ultricies. Maecenas bibendum turpis et ligula elementum, sed vestibulum risus varius. Suspendisse dui justo, hendrerit vestibulum tristique id, egestas at leo. Pellentesque feugiat libero vel eros scelerisque luctus dapibus eget dui.",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 5,
+        title: "Lenovo - Black",
+        img: "img/lenovo (1).jpg",
+        price: 10,
+        company: "GOOGLE",
+        info: 
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam non dolor sed risus volutpat semper et eget eros. Aenean at risus euismod, fringilla odio id, feugiat nisl. Aenean sollicitudin leo ac sem mollis pharetra id quis ipsum. Suspendisse potenti. Nullam hendrerit mauris in lectus faucibus mattis at sed turpis. Ut ac suscipit odio, pellentesque commodo dui. Proin volutpat viverra dui, eget cursus mi accumsan eu. Aenean quis eros scelerisque, finibus mi sit amet, cursus diam. Pellentesque varius nibh nec lacus rhoncus commodo. Fusce suscipit porttitor magna eget volutpat. Cras in augue et metus rhoncus condimentum. Fusce sagittis elit et tempus ultricies. Maecenas bibendum turpis et ligula elementum, sed vestibulum risus varius. Suspendisse dui justo, hendrerit vestibulum tristique id, egestas at leo. Pellentesque feugiat libero vel eros scelerisque luctus dapibus eget dui.",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 6,
+        title: "MacBook - Black",
+        img: "img/macbook (1).jpg",
+        price: 10,
+        company: "GOOGLE",
+        info: 
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam non dolor sed risus volutpat semper et eget eros. Aenean at risus euismod, fringilla odio id, feugiat nisl. Aenean sollicitudin leo ac sem mollis pharetra id quis ipsum. Suspendisse potenti. Nullam hendrerit mauris in lectus faucibus mattis at sed turpis. Ut ac suscipit odio, pellentesque commodo dui. Proin volutpat viverra dui, eget cursus mi accumsan eu. Aenean quis eros scelerisque, finibus mi sit amet, cursus diam. Pellentesque varius nibh nec lacus rhoncus commodo. Fusce suscipit porttitor magna eget volutpat. Cras in augue et metus rhoncus condimentum. Fusce sagittis elit et tempus ultricies. Maecenas bibendum turpis et ligula elementum, sed vestibulum risus varius. Suspendisse dui justo, hendrerit vestibulum tristique id, egestas at leo. Pellentesque feugiat libero vel eros scelerisque luctus dapibus eget dui.",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id:7,
+        title: "Predator - Black",
+        img: "img/predator (1).jpg",
+        price: 10,
+        company: "GOOGLE",
+        info: 
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam non dolor sed risus volutpat semper et eget eros. Aenean at risus euismod, fringilla odio id, feugiat nisl. Aenean sollicitudin leo ac sem mollis pharetra id quis ipsum. Suspendisse potenti. Nullam hendrerit mauris in lectus faucibus mattis at sed turpis. Ut ac suscipit odio, pellentesque commodo dui. Proin volutpat viverra dui, eget cursus mi accumsan eu. Aenean quis eros scelerisque, finibus mi sit amet, cursus diam. Pellentesque varius nibh nec lacus rhoncus commodo. Fusce suscipit porttitor magna eget volutpat. Cras in augue et metus rhoncus condimentum. Fusce sagittis elit et tempus ultricies. Maecenas bibendum turpis et ligula elementum, sed vestibulum risus varius. Suspendisse dui justo, hendrerit vestibulum tristique id, egestas at leo. Pellentesque feugiat libero vel eros scelerisque luctus dapibus eget dui.",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 8,
+        title: "Samsung - Black",
+        img: "img/samsung-laptop (1).jpg",
+        price: 10,
+        company: "GOOGLE",
         info: 
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam non dolor sed risus volutpat semper et eget eros. Aenean at risus euismod, fringilla odio id, feugiat nisl. Aenean sollicitudin leo ac sem mollis pharetra id quis ipsum. Suspendisse potenti. Nullam hendrerit mauris in lectus faucibus mattis at sed turpis. Ut ac suscipit odio, pellentesque commodo dui. Proin volutpat viverra dui, eget cursus mi accumsan eu. Aenean quis eros scelerisque, finibus mi sit amet, cursus diam. Pellentesque varius nibh nec lacus rhoncus commodo. Fusce suscipit porttitor magna eget volutpat. Cras in augue et metus rhoncus condimentum. Fusce sagittis elit et tempus ultricies. Maecenas bibendum turpis et ligula elementum, sed vestibulum risus varius. Suspendisse dui justo, hendrerit vestibulum tristique id, egestas at leo. Pellentesque feugiat libero vel eros scelerisque luctus dapibus eget dui.",
         inCart: false,
